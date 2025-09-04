8aa4b0b856960312046515865c157a49a08c8ab7
DmitriyALEX <doiroffical@gmail.com>
2025-09-04 20:18:11 +0300

    add task solution

---
+++ src/pages/notFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">Page not found</h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
